5205efc273df0cd557d2cff6f4016f20597a8abe
dcarbajosa <dcarbajosa@gmail.com>
2015-12-18 11:28:56 -0500

    Commit chef-repo for the 1st time

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,9 @@
+default["apache"]["sites"]["chefnode.dc.hm"] = { "site_title" => "Chef Node website is coming soon", "port" => 80, "domain" => "chefnode.dc.hm" }
+default["apache"]["sites"]["chefnodeb.dc.hm"] = {"site_title" => "Chef Node B, de Bestia, website is coming soon",  "port" => 80, "domain" => "chefnodeb.dc.hm" }
+
+case node["platform"]
+when "centos"
+	default["apache"]["package"] = "httpd"
+when "ubuntu"
+	default["apache"]["package"] = "apache2"
+end
